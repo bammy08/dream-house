43398ea5a18467f60a5ca728abec683d29146e36
bammy08 <olusolabamidele33@gmail.com>
2023-02-11 07:41:21 +0100

    created sign up with google

--- src/components/SignUpWithGoogle.jsx
+++ src/components/SignUpWithGoogle.jsx
@@ -1,13 +1,48 @@
 import React from 'react';
 import { FcGoogle } from 'react-icons/fc';
 import { motion } from 'framer-motion';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { db } from '../firebase';
+import { doc, getDoc, serverTimestamp, setDoc } from 'firebase/firestore';
 
 const SignUpWithGoogle = () => {
+  const navigate = useNavigate();
+
+  const onGoogleClick = async () => {
+    try {
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+
+      // check for the user
+
+      const docRef = doc(db, 'users', user.uid);
+      const docSnap = await getDoc(docRef);
+
+      if (!docSnap.exists()) {
+        await setDoc(docRef, {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp(),
+        });
+      }
+      toast.success('google signed in successfully');
+
+      navigate('/');
+    } catch (error) {
+      toast.error('Could not authorize with Google');
+    }
+  };
+
   return (
     <motion.button
       whileTap={{ scale: 0.75 }}
       type="button"
       className="flex items-center justify-center w-full bg-red-700 text-white px-7 py-3 uppercase text-sm font-medium hover:bg-red-800 active:bg-red-900 shadow-md hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out rounded"
+      onClick={onGoogleClick}
     >
       <FcGoogle className="text-2xl  bg-white rounded-full mr-2" />
       Continue with Google
