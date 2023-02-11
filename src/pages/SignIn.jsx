6df81efc46e8d13746947a2185ad65e9b3c24ee5
bammy08 <olusolabamidele33@gmail.com>
2023-02-11 09:52:38 +0100

    created the sign in page

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import logo from '../assests/login.webp';
 import { AiFillEyeInvisible, AiFillEye } from 'react-icons/ai';
-import { Link } from 'react-router-dom';
+import { Link, Navigate, useNavigate } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import SignUpWithGoogle from '../components/SignUpWithGoogle';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { toast } from 'react-toastify';
 
 const SignIn = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -13,11 +15,30 @@ const SignIn = () => {
   });
 
   const { email, password } = formData;
+  const navigate = useNavigate();
   const changeValue = (e) => {
     setFormData((prevState) => ({
       ...prevState,
       [e.target.id]: e.target.value,
     }));
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const auth = getAuth();
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      if (userCredential.user) {
+        toast.success('signed in successfully');
+        navigate('/');
+      }
+    } catch (error) {
+      toast.error('Bad user credentials');
+    }
   };
 
   return (
@@ -28,7 +49,7 @@ const SignIn = () => {
           <img src={logo} alt="key" className="w-full rounded-2xl" />
         </div>
         <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
-          <form>
+          <form onSubmit={onSubmit}>
             <input
               className="w-full px-4 py-2 text-xl text-gray-700 border-gray-300 rounded transition ease-in-out mb-6"
               type="email"
